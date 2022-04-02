e5371b80a0e9bd32d4d1dc180d0d1605d8cd231e
putriliza <putriliza05@gmail.com>
2022-04-02 20:50:06 +0700

    pake dict

--- src/inputPuzzle.py
+++ src/inputPuzzle.py
@@ -9,11 +9,11 @@ def inputFromFile(filename):
     puzzle = np.zeros((4,4), dtype=int)
     for i in range(0, 16):
         puzzle[i // 4, i % 4] = int(content[i])
-    return puzzle
+    return np.array(puzzle)
 
 def inputFromRandom():
     tmp = random.sample(range(0, 16), 16)
     puzzle = np.zeros((4,4), dtype=int)
     for i in range(0, 16):
         puzzle[i // 4, i % 4] = tmp[i]
-    return puzzle
+    return np.array(puzzle)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np
 
 import copy
 from inputPuzzle import *
@@ -6,22 +7,22 @@ from queue import PriorityQueue
 
 
 class node:	
-	def __init__(self, puzzle, emptyTilePos, cost, level, parent):
-		self.puzzle = puzzle
-		self.parent = parent
+	def __init__(self, matrix, emptyTilePos, cost, level, parent):
+		self.matrix = matrix
 		self.emptyTilePos = emptyTilePos
 		self.cost = cost
 		self.level = level
+		self.parent = parent
 
 	def __lt__(self, nxt):
 		return self.cost < nxt.cost
 
-def calculateCost(puzzle) -> int:
+def calculateCost(matrix) -> int:
 	cost = 0
 	for i in range(n):
 		for j in range(n):
-			if (puzzle[i][j] != final[i][j]):
-				if puzzle[i][j] == 16:
+			if (matrix[i][j] != final[i][j]):
+				if matrix[i][j] == 16:
 					continue
 				cost += 1
 				
@@ -29,27 +30,31 @@ def calculateCost(puzzle) -> int:
 
 def newNode(newTilePos, liveNode) -> node:
 
-	new_puzzle = copy.deepcopy(liveNode.puzzle)
+	newMatrix = copy.deepcopy(liveNode.matrix)
 
 	x1, y1 = liveNode.emptyTilePos
 	x2, y2 = newTilePos
-	new_puzzle[x1][y1], new_puzzle[x2][y2] = new_puzzle[x2][y2], new_puzzle[x1][y1]
+	newMatrix[x1][y1], newMatrix[x2][y2] = newMatrix[x2][y2], newMatrix[x1][y1]
 
-	cost = calculateCost(new_puzzle)
+	cost = calculateCost(newMatrix)
 
 	level = liveNode.level + 1
 	cost = cost + level		# g + f
-	new_node = node(new_puzzle, newTilePos,
-					cost, level, liveNode)
-	return new_node
 
-def printPuzzle(puzzle):
+	if (newMatrix.tobytes() in visitedMatrix):
+		return None
+	else:
+		visitedMatrix[newMatrix.tobytes()] = True
+		new_node = node(newMatrix, newTilePos, cost, level, liveNode)
+		return new_node
+
+def printmatrix(matrix):
     for i in range(n):
         for j in range(n):
-            if puzzle[i][j] == 16:
+            if matrix[i][j] == 16:
                 print("-", end=" ")
             else:
-                print("%d " % (puzzle[i][j]), end = " ")
+                print("%d " % (matrix[i][j]), end = " ")
 			
         print()
 
@@ -62,7 +67,7 @@ def printRute(root):
 		return
 	printRute(root.parent)
 	print(f"Cost: {root.cost}, Level: {root.level}")
-	printPuzzle(root.puzzle)
+	printmatrix(root.matrix)
 	print()
 
 def solve(initial, emptyTilePos):
@@ -88,34 +93,36 @@ def solve(initial, emptyTilePos):
 				
 			if isValid(newTilePos[0], newTilePos[1]):
 				child = newNode(newTilePos, liveNode)
-				global totalNodes
-				totalNodes += 1
-				pq.put(child)
+				if child != None:
+					pq.put(child)
 
-def getXPos2D(puzzle, x):
+					global totalNodes
+					totalNodes += 1
+
+def getXPos2D(matrix, x):
     for i in range(n):
         for j in range(n):
-            if puzzle[i][j] == x:
+            if matrix[i][j] == x:
                 return [i, j]
 
-def getXPosition(puzzle, x):
-	x1, x2 = getXPos2D(puzzle, x)
+def getXPosition(matrix, x):
+	x1, x2 = getXPos2D(matrix, x)
 	return x1 * n + x2
 
-def SumKurangIplusX(puzzle):
+def SumKurangIplusX(matrix):
     SumKurangI = 0
     for i in range(1, n**2+1):
         kurangI = 0
         for j in range (1, i):
-            if getXPosition(puzzle, j) > getXPosition(puzzle, i):
+            if getXPosition(matrix, j) > getXPosition(matrix, i):
                 kurangI += 1
         SumKurangI += kurangI
-    x1, x2 = getXPos2D(puzzle, 16)
+    x1, x2 = getXPos2D(matrix, 16)
     X = (x1 + x2) % 2
     return SumKurangI + X
 
-def isReachable(puzzle):
-    return SumKurangIplusX(puzzle) % 2 == 0
+def isReachable(matrix):
+    return SumKurangIplusX(matrix) % 2 == 0
 
 
 n = 4
@@ -125,12 +132,14 @@ col = [ 0, -1, 0, 1 ]
 
 totalNodes = 0
 
+visitedMatrix = {}
+
 initial = inputFromFile("t4.txt")
 
-final = [[ 1, 2, 3, 4],
+final = np.array([[ 1, 2, 3, 4],
 		 [5, 6, 7, 8],
 		 [9, 10, 11 , 12],
-		 [13, 14, 15, 16]]
+		 [13, 14, 15, 16]])
 
 emptyTilePos = getXPos2D(initial, 16)
 
