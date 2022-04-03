56588ed8a73874c2291c66f5755f62603acf0c63
putriliza <putriliza05@gmail.com>
2022-04-03 14:24:59 +0700

    looping main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
+from mimetypes import init
 import time
+import os.path
 
 from inputPuzzle import *
 from node import *
@@ -19,37 +21,54 @@ print("""
                                                                  
 """)
 
-print("You can use initial puzzle from")
-print("1. File")
-print("2. Randomizer")
-opsi = int(input("Choose initial puzzle from: "))
-if opsi == 1:
-	filename = input("Enter filename: ")
-	initial = inputFromFile(filename)
-elif opsi == 2:
-	initial = inputFromRandom()
-else:
-	print("Invalid input")
+isRun = True
 
-print()
-if (opsi == 1 or opsi == 2):
-	print("Initial Matrix:")
-	printmatrix(initial)
-	print()
-	for i in range(1, n**2+1):
-		print(f"{i} = {kurangI(initial, i)}")
-	print(f"SumKurangI+X = {SumKurangIplusX(initial)}")
-	print()
+while(isRun):
+	print("You can use initial puzzle from")
+	print("1. File")
+	print("2. Randomizer")
+	opsi = int(input("Choose initial puzzle from: "))
+	inputExist = False
 
-	if (isReachable(initial)):
-		print("THIS PUZZLE IS SOLVEABLE:D\n")
-		print("Please wait for a while...")
-		start = time.time()
-		solution, totalNodes = solve(initial)
-		end = time.time()
-		printRute(solution)
-		print("---------------------------------------------------")
-		print(f"Time elapsed 		: {end-start} seconds")
-		print(f"Total nodes raised 	: {totalNodes}")
+	if opsi == 1:
+		filename = input("Enter filename: ")
+		if (os.path.exists("../test/" + filename)):
+			initial = inputFromFile(filename)
+			inputExist = True
+		else:
+			print("File not found")
+	elif opsi == 2:
+		initial = inputFromRandom()
+		inputExist = True
 	else:
-		print("THIS PUZZLE IS NOT SOLVEABLE :(")
+		print("Invalid input")
+
+	print()
+	if (inputExist):
+		print("Initial Puzzle:")
+		printmatrix(initial)
+		print()
+		for i in range(1, n**2+1):
+			print(f"{i} = {kurangI(initial, i)}")
+		print(f"SumKurangI+X = {SumKurangIplusX(initial)}")
+		print()
+
+		if (isReachable(initial)):
+			print("THIS PUZZLE IS SOLVEABLE:D\n")
+			print("Please wait for a while...")
+			start = time.time()
+			solution, totalNodes = solve(initial)
+			end = time.time()
+			printRute(solution)
+			print("---------------------------------------------------")
+			print(f"Time elapsed 		: {end-start} seconds")
+			print(f"Total nodes raised 	: {totalNodes}")
+		else:
+			print("THIS PUZZLE IS NOT SOLVEABLE :(")
+	
+	loop = input("Wanna try another puzzle? (y/n) ")
+	if (loop.upper() == "Y"):
+		isRun = True
+	else:
+		isRun = False
+	
